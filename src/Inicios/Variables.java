1be53011b461087f1e92d62744ba9bedfb0f1e08
M4rced <marcedlll12@gmail.com>
2023-03-06 00:09:30 -0500

    Declaración, inicialización e impresion de la variable edad

---
+++ src/Inicios/Variables.java
@@ -0,0 +1,10 @@
+package Inicios;
+
+public class Variables {
+    public static void main(String[] args) {
+        int edad = 35; //Declaracion e inicialización de variable en una única línea
+        System.out.println("La edad uno es:"+ edad); //Concanetación de un mensaje y la impresion del valor de la variable
+        edad=75; // Inicializar una varibale
+        System.out.println("La edad dos es:"+ edad);// (System.out) es la función para impresion en la consola
+    }
+}
